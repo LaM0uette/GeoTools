1d4203e3f905f051dd7be7c8031c08127dc6a417
LaM0uette <lam0uette.dev@gmail.com>
2022-05-20 16:18:11 +0200

    up

--- Views/MainView.xaml.cs
+++ Views/MainView.xaml.cs
@@ -24,6 +24,7 @@ public partial class MainView : UserControl
     //     
     //     TestGrid.Children.Add(btn);
     // }
+    
     private void BtnTiMenu_OnClick(object sender, RoutedEventArgs e)
     {
         TiExportGrace.IsSelected = true;
