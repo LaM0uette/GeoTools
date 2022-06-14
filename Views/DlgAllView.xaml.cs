471174ae0f5a9e6aa954cefc957f5420a1210764
LaM0uette <lam0uette.dev@gmail.com>
2022-06-14 14:11:45 +0200

    ajout du bouton retour au menu

--- Views/DlgAllView.xaml.cs
+++ Views/DlgAllView.xaml.cs
@@ -1,11 +1,21 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Controls;
 
 namespace GeoTools.Views;
 
 public partial class DlgViewAll : UserControl
 {
+    private readonly MyViewModel _viewModel;
     public DlgViewAll()
     {
         InitializeComponent();
+        _viewModel = new MyViewModel();
     }
+}
+
+// INotifyPropertyChanged notifies the View of property changes, so that Bindings are updated.
+sealed class MyViewModel : INotifyPropertyChanged
+{
+    
 }
