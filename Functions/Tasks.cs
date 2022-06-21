3c1891427ec61cf57c1ddb0f2eff997433ed140e
LaM0uette <lam0uette.dev@gmail.com>
2022-06-21 14:20:15 +0200

    user in task

---
+++ Functions/Tasks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace GeoTools.Functions;
+
+public class Tasks
+{
+    private static string _guid ;
+
+    public static string Guid
+    {
+        get => _guid;
+        set => _guid = Environment.UserName;
+    }
+}
+
--- Model/User.cs
+++ Model/User.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using GeoTools.Functions;
 
 namespace GeoTools.Model;
 
 public class User
 {
-    public string Guid { get; } = Environment.UserName;
+    public string Guid = Tasks.Guid;
     public bool Admin { get; set; }
     public string Prenom { get; set; }
     public string Nom { get; set; }
