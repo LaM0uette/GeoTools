0ff589ce0cf38f237860edcc89679865f09f74d2
LaM0uette <lam0uette.dev@gmail.com>
2022-06-22 12:09:43 +0200

    maj sql req

--- sql/test_data.sql
+++ sql/test_data.sql
@@ -7,28 +7,38 @@ SELECT *
 FROM t_dlg;
 
 INSERT INTO t_dlg (dl_rc_id, dl_proj_us_id, dl_exe_us_id, dl_date_init, dl_ph_id, dl_te_id, dl_livraison, dl_version)
-VALUES (1, 'XD5965', 'XD5965', CURRENT_DATE, 2, 3, 1, 1),
-       (1, 'XD5965', 'XD5965', CURRENT_DATE, 2, 3, 1, 2),
-       (1, 'XD5965', 'XD5965', CURRENT_DATE, 2, 2, 2, 1),
-       (2, 'XD5965', 'XD5965', CURRENT_DATE, 3, 2, 2, 1),
-       (3, 'XD5965', 'XD5965', CURRENT_DATE, 3, 3, 1, 1),
-       (4, 'XD5965', 'XD5965', CURRENT_DATE, 3, 3, 1, 2),
-       (4, 'XD5965', 'XD5965', CURRENT_DATE, 2, 2, 2, 1);
+VALUES (1, 1, 2, CURRENT_DATE, 2, 3, 1, 1),
+       (1, 1, 2, CURRENT_DATE, 2, 3, 1, 2),
+       (1, 1, 2, CURRENT_DATE, 2, 2, 2, 1),
+       (2, 1, 2, CURRENT_DATE, 3, 2, 2, 1),
+       (3, 1, 2, CURRENT_DATE, 3, 3, 1, 1),
+       (4, 1, 2, CURRENT_DATE, 3, 3, 1, 2),
+       (4, 1, 2, CURRENT_DATE, 2, 2, 2, 1);
 
 -- Procédure
-
-create or replace procedure add_export_to_dlg(dlg_id int, etat_id int)
+create or replace function add_export_to_dlg(dlg_id int, etat_id int) returns void
 as
-$$
+$BODY$
 begin
-    INSERT INTO t_exports (ex_dl_id, ex_et_id, ex_date)
-    VALUES (dlg_id, etat_id, CURRENT_TIMESTAMP);
+    INSERT INTO t_exports (ex_dl_id, ex_et_id)
+    VALUES (dlg_id, etat_id);
 end;
-$$;
+$BODY$
+    LANGUAGE plpgsql VOLATILE
+                     COST 100;
+
+SELECT add_export_to_dlg(2, 5);
 
 -- Trigger pour mettre dans la t_export
+create or replace function tr_add_export_to_dlg() RETURNS trigger AS $BODY$
+begin
+    INSERT INTO "GeoTools"."t_exports" (ex_dl_id, ex_et_id)
+    VALUES (NEW.dl_id, 1);
+    RETURN NEW;
+end;
+$BODY$
+    LANGUAGE plpgsql VOLATILE
+                     COST 100;
 
-create or replace trigger ai_t_dlg
-    after insert
-    on t_dlg
-execute procedure add_export_to_dlg(dlg_id := :NEW.dl_id, etat_id := 0, proj_id := _, exe_id := NULL);
+create trigger tr_add_export_to_dlg after insert on "GeoTools"."t_dlg"
+FOR EACH ROW EXECUTE PROCEDURE "GeoTools".tr_add_export_to_dlg();
