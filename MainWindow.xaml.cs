543be6cc8de73b234a2df95a9233c16544d0baf6
LaM0uette <lam0uette.dev@gmail.com>
2022-06-23 14:47:40 +0200

    teste procédure postgres

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,41 +1,58 @@
-﻿using System.Data.SQLite;
+﻿using System.Data;
+using System.Data.SQLite;
 using System.Threading.Tasks;
 using GeoTools.Model;
 using GeoTools.Functions;
+using Npgsql;
 
 namespace GeoTools
 {
-    
     public partial class MainWindow
     {
-        public static SQLiteConnection Connection = new ();
-        public static User UserSession = new ();
+        public static SQLiteConnection Connection = new();
+        public static User UserSession = new();
 
         public MainWindow()
         {
             Connection = new SQLiteConnection("Data Source=T:\\- 4 Suivi Appuis\\25_BDD\\MyDLG\\bdd.sqlite");
             Connection.Open();
-            
+
+            var cs = "HOST=BORDEAUX04;Username=postgres;Password=INEO_Infracom_33;Database=sig";
+            var con = new NpgsqlConnection(cs);
+
+            con.Open();
+            var tr = con.BeginTransaction();
+
+            // var sql = "get_dlg_by_date";
+
+            // var sql = "SELECT * FROM \"GeoTools\".v_dlg";
+            var sql = "SELECT * FROM \"GeoTools\".get_dlg_by_date('2022-06-23')";
+            sql = "SELECT * FROM \"GeoTools\".add_dlg('XD5965', 'BIVO', CURRENT_DATE, 'EXE', 'TRANSPORT ET DISTRIBUTION', 2, 4)";
+            var cmd = new NpgsqlCommand(sql, con);
+            tr.Commit();
+
+            NpgsqlDataReader pgreader = cmd.ExecuteReader();
+
+
             SetUserParameters();
         }
-        
+
         private static void SetUserParameters()
         {
             string req = $"SELECT * FROM t_users WHERE us_guid='{Tasks.GetUserSession()}'";
 
             SQLiteDataReader cdReader = Tasks.GetData(cmd: req);
-            
+
             while (cdReader.Read())
             {
                 UserSession.Prenom = $"{cdReader["us_prenom"]}";
                 UserSession.Nom = $"{cdReader["us_nom"]}";
-                
+
                 if (cdReader.GetByte(cdReader.GetOrdinal("us_admin")) == 1)
                 {
                     UserSession.Admin = true;
                 }
             }
         }
-        
     }
 }
--- sql/test_data.sql
+++ sql/test_data.sql
@@ -22,12 +22,12 @@ create or replace function _add_dlg(proj_us_id int, rc_id int, date_init DATE, p
 as
 $BODY$
 begin
-    INSERT INTO t_dlg (dl_proj_us_id, dl_rc_id, dl_date_init,
+    INSERT INTO "GeoTools".t_dlg (dl_proj_us_id, dl_rc_id, dl_date_init,
                        dl_ph_id, dl_te_id, dl_livraison, dl_version)
     SELECT proj_us_id, rc_id, date_init, ph_id, te_id, livraison, version
     WHERE NOT EXISTS(SELECT dl_id
-                     FROM t_dlg
-                     WHERE dl_rc_id = dl_rc_id
+                     FROM "GeoTools".t_dlg
+                     WHERE dl_rc_id = rc_id
                        AND dl_ph_id = ph_id
                        AND dl_te_id = te_id
                        AND dl_livraison = livraison
@@ -47,19 +47,19 @@ as
 $BODY$
 declare
     proj_us_id int = (SELECT us_id
-                      FROM t_users
+                      FROM "GeoTools".t_users
                       WHERE us_guid = proj);
     rc_id      int = (SELECT rc_id
-                      FROM data.l_refcode
+                      FROM "data".l_refcode
                       WHERE rc_refcode3 = refcode3);
     ph_id      int = (SELECT ph_id
-                      FROM l_phases
+                      FROM "GeoTools".l_phases
                       WHERE ph_nom = ph);
     te_id      int = (SELECT te_id
-                      FROM l_type_export
+                      FROM "GeoTools".l_type_export
                       WHERE te_nom = te);
 begin
-    INSERT INTO t_dlg (dl_proj_us_id, dl_rc_id, dl_date_init,
+    INSERT INTO "GeoTools".t_dlg (dl_proj_us_id, dl_rc_id, dl_date_init,
                        dl_ph_id, dl_te_id, dl_livraison, dl_version)
     SELECT proj_us_id,
            rc_id,
@@ -69,7 +69,7 @@ begin
            livraison,
            version
     WHERE NOT EXISTS(SELECT dl_id
-                     FROM t_dlg
+                     FROM "GeoTools".t_dlg
                      WHERE dl_rc_id = rc_id
                        AND dl_ph_id = ph_id
                        AND dl_te_id = te_id
@@ -92,7 +92,7 @@ create or replace function add_export_to_dlg(dlg_id int, etat_id int) returns vo
 as
 $BODY$
 begin
-    INSERT INTO t_exports (ex_dl_id, ex_et_id)
+    INSERT INTO "GeoTools".t_exports (ex_dl_id, ex_et_id)
     VALUES (dlg_id, etat_id);
 end;
 $BODY$
@@ -129,104 +129,80 @@ FROM t_dlg dl;
 
 -- ...
 -- v_dlg
-SELECT dl_id                       as id,
-       usp.us_guid                 as guid_projeteur,
-       usp.us_nom || usp.us_prenom as projeteur,
-       use.us_guid                 as guid_executant,
-       use.us_nom || use.us_prenom as executant,
-       rc.rc_refcode1              as refcode1,
-       rc.rc_refcode2              as refcode2,
-       rc.rc_refcode3              as refcode3,
-       dl.dl_date_init             as date_initial,
-       ph.ph_nom                   as phase,
-       te.te_nom                   as type_export,
-       dl.dl_livraison             as livraison,
-       dl.dl_version               as version,
-       (SELECT ex_id
-        FROM t_exports
-        WHERE ex_dl_id = dl.dl_id
-        ORDER BY ex_date DESC
-        LIMIT 1) as id_export,
-    (SELECT ex_et_id
-        FROM t_exports
-        WHERE ex_dl_id = dl.dl_id
-        ORDER BY ex_date DESC
-        LIMIT 1) as id_export
-FROM t_dlg dl
-         INNER JOIN t_users usp
-                    ON usp.us_id = dl.dl_proj_us_id
-         INNER JOIN t_users use
-                    ON use.us_id = dl.dl_proj_us_id
-         INNER JOIN data.l_refcode rc
-                    ON rc.rc_id = dl.dl_rc_id
-         INNER JOIN l_phases ph
-                    ON ph.ph_id = dl.dl_ph_id
-         INNER JOIN l_type_export te
-                    ON te.te_id = dl.dl_te_id;
+create or replace view "GeoTools".v_dlg as
+WITH dlg AS (SELECT dl_id                              as id,
+                    usp.us_guid                        as guid_projeteur,
+                    usp.us_nom || ' ' || usp.us_prenom as projeteur,
+                    use.us_guid                        as guid_executant,
+                    use.us_nom || ' ' || use.us_prenom as executant,
+                    rc.rc_refcode1                     as refcode1,
+                    rc.rc_refcode2                     as refcode2,
+                    rc.rc_refcode3                     as refcode3,
+                    dl.dl_date_init                    as date_initial,
+                    ph.ph_nom                          as phase,
+                    te.te_nom                          as type_export,
+                    dl.dl_livraison                    as livraison,
+                    dl.dl_version                      as version,
+                    (SELECT ex_id
+                     FROM "GeoTools".t_exports
+                     WHERE ex_dl_id = dl.dl_id
+                     ORDER BY ex_date DESC
+                     LIMIT 1)                          as id_export
+             FROM "GeoTools".t_dlg dl
+                      LEFT JOIN "GeoTools".t_users usp
+                                ON usp.us_id = dl.dl_proj_us_id
+                      LEFT JOIN "GeoTools".t_users use
+                                ON use.us_id = dl.dl_exe_us_id
+                      INNER JOIN data.l_refcode rc
+                                 ON rc.rc_id = dl.dl_rc_id
+                      INNER JOIN "GeoTools".l_phases ph
+                                 ON ph.ph_id = dl.dl_ph_id
+                      INNER JOIN "GeoTools".l_type_export te
+                                 ON te.te_id = dl.dl_te_id)
+SELECT dlg.*, et.et_code as code_etat, et.et_nom as nom_etat, et.et_color as couleur_etat, ex.ex_date as date_etat
+FROM dlg
+         INNER JOIN "GeoTools".t_exports ex
+                    ON ex.ex_id = dlg.id_export
+         INNER JOIN "GeoTools".l_etats et
+                    ON et.et_id = ex.ex_et_id;
+
+-- v_exports
+create or replace view "GeoTools".v_exports as
+SELECT ex.ex_id as id,
+       ex.ex_dl_id as dlg_id,
+       et.et_code as code_etat,
+       et.et_nom as nom_etat,
+       et_code as couleur_etat
+FROM "GeoTools".t_exports ex
+         INNER JOIN "GeoTools".l_etats et
+                    ON et.et_id = ex.ex_et_id
+ORDER BY ex.ex_dl_id, ex.ex_id;
+
+
+-- ...
+-- Procédures
+create or replace function get_dlg_exports(dlg int) returns setof "GeoTools".v_exports
+as
+$BODY$
+begin
+    RETURN QUERY (SELECT * FROM "GeoTools".v_exports WHERE dlg_id = dlg ORDER BY dlg_id, id);
+end;
+$BODY$
+    LANGUAGE plpgsql VOLATILE
+                     COST 100;
+
+SELECT * FROM get_dlg_exports(2);
 
 
 
+create or replace function get_dlg_by_date(dt date) returns setof "GeoTools".v_dlg
+as
+$BODY$
+begin
+    RETURN QUERY (SELECT * FROM "GeoTools".v_dlg WHERE DATE(date_initial) = dt);
+end
+$BODY$
+    LANGUAGE plpgsql VOLATILE
+                     COST 100;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-SELECT dl_id                       as id,
-       usp.us_guid                 as guid_projeteur,
-       usp.us_nom || usp.us_prenom as projeteur,
-       use.us_guid                 as guid_executant,
-       use.us_nom || use.us_prenom as executant,
-       rc.rc_refcode1              as refcode1,
-       rc.rc_refcode2              as refcode2,
-       rc.rc_refcode3              as refcode3,
-       dl.dl_date_init             as date_initial,
-       ph.ph_nom                   as phase,
-       te.te_nom                   as type_export,
-       dl.dl_livraison             as livraison,
-       dl.dl_version               as version,
-       (SELECT ex_id
-        FROM t_exports
-        WHERE ex_dl_id = dl.dl_id
-        ORDER BY ex_date DESC
-        LIMIT 1)
-FROM t_dlg dl
-         INNER JOIN t_users usp
-                    ON usp.us_id = dl.dl_proj_us_id
-         INNER JOIN t_users use
-                    ON use.us_id = dl.dl_proj_us_id
-         INNER JOIN data.l_refcode rc
-                    ON rc.rc_id = dl.dl_rc_id
-         INNER JOIN l_phases ph
-                    ON ph.ph_id = dl.dl_ph_id
-         INNER JOIN l_type_export te
-                    ON te.te_id = dl.dl_te_id;
+SELECT * FROM get_dlg_by_date('2022-06-23');
