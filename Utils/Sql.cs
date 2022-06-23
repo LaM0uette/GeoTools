f663cae1bb2c558d60bb92f8d1dadc25435507a8
LaM0uette <lam0uette.dev@gmail.com>
2022-06-23 15:40:53 +0200

    ajout des logins

--- Utils/Sql.cs
+++ Utils/Sql.cs
@@ -1,24 +1,28 @@
 ﻿using Npgsql;
+using GeoTools.Configs;
 
 namespace GeoTools.Utils;
 
 public class Sql
 {
-    
     public struct SqlCommand
     {
         public NpgsqlConnection Connection;
         public NpgsqlTransaction Transaction;
     }
-    
-    public static SqlCommand Connect() 
+
+    public static SqlCommand Connect()
     {
-        var connexionString = "HOST=BORDEAUX04;Username=postgres;Password=INEO_Infracom_33;Database=sig";
+        var connexionString = $"HOST={Login.PgHost};" +
+                              $"Username={Login.PgUser};" +
+                              $"Password={Login.PgPassword};" +
+                              $"Database={Login.PgDatabase}";
+        
         var connexion = new NpgsqlConnection(connexionString);
         connexion.Open();
-        
+
         var transaction = connexion.BeginTransaction();
         
-        return new SqlCommand{Connection = connexion, Transaction = transaction};
+        return new SqlCommand {Connection = connexion, Transaction = transaction};
     }
 }
