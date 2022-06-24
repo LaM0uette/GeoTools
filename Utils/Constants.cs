29c97f44c597144e8c790f1821368678ef3a3a46
LaM0uette <lam0uette.dev@gmail.com>
2022-06-24 17:02:30 +0200

    refactor

--- Utils/Constants.cs
+++ Utils/Constants.cs
@@ -3,7 +3,7 @@
 public static class Constants
 {
     //...
-    // WidgetsSize
+    // Dlg created
     public const int DlgWith = 300;
     public const byte DlgHeight = 120;
     public const byte DlgLargeColumnWidth = 140;
