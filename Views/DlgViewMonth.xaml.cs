1e524184a0a31e8b5c5c891041c895f367ee38b3
LaM0uette <lam0uette.dev@gmail.com>
2022-06-27 12:17:48 +0200

    add dlg by month

--- Views/DlgViewMonth.xaml.cs
+++ Views/DlgViewMonth.xaml.cs
@@ -23,7 +23,7 @@ public partial class DlgViewMonth : UserControl
         InstanceDlgViewMonth = this;
         InitializeComponent();
         
-        CreateBtnDlgMonth(year:2022, month:12);
+        CreateBtnDlgMonth(year:2022, month:6);
     }
 
     public void CreateBtnDlgMonth(int year, byte month)
@@ -31,9 +31,7 @@ public partial class DlgViewMonth : UserControl
         int yearEnd = year;
         byte monthEnd = (byte)(month + 1);
         if (monthEnd == 13){ yearEnd++; monthEnd = 1;}
-        
-        var style = FindResource("ButtonDLGTemp") as Style;
-        
+
         DateTime startDate = new DateTime(day: 1, month: month, year: year);
         DateTime endDate = new DateTime(day: 1, month: monthEnd, year: yearEnd).AddDays(-1);
         
@@ -44,112 +42,70 @@ public partial class DlgViewMonth : UserControl
         {
             foreach (DateTime date in Tasks.EachDay(from:Tasks.GetDayOfWeek(week:firstWeeks + i, year:year), to:Tasks.GetDayOfWeek(week:firstWeeks + i, year:year, DayOfWeek.Friday)))
             {
-                string nameOfWeek = Tasks.FistLetterUpper(date.ToString("dddd", lang));
                 int col = (int)date.DayOfWeek - 1;
-                
-                StackPanel stackPanel = new StackPanel()
-                {
-                    Orientation = Orientation.Vertical,
-                    HorizontalAlignment = HorizontalAlignment.Center
-                };
-                
-                Brush foreground = dateNow == date ? Brushes.White : FindResource("RgbM2") as Brush;
-                
-                Label lbJourNom = makeLabel(content: nameOfWeek, foreground: foreground);
-                Label lbNumJour = makeLabel(content: $"{date.Day}", foreground: foreground);
-                
-                Grid gridDlg = new Grid();
-                int dlgRow = 0;
-                
-                NpgsqlDataReader cdReader = Sql.GetDlgByDate(date: date.ToString("yyyy-MM-dd"));
-                while (cdReader.Read())
-                {
-                    Button button = Widget.MakeBtnDlg(cdReader: cdReader, style: style);
-                    Widget.SetElementGrid(button, row: dlgRow);
-                
-                    gridDlg.RowDefinitions.Add(new RowDefinition());
-                    gridDlg.Children.Add(button);
-                    dlgRow++;
-                }
-                
-                cdReader.Close();
-                
-                stackPanel.Children.Add(lbJourNom);
-                stackPanel.Children.Add(lbNumJour);
-                stackPanel.Children.Add(gridDlg);
-
+                StackPanel stackPanel = makePanel(date: date);
                 Widget.SetElementGrid(stackPanel, row:i, column:col);
                 GridMonth.Children.Add(stackPanel);
             }
-
         }
-        // foreach (DateTime date in Tasks.EachDay(from: startDate, to: endDate))
-        // {
-        //     string nameOfWeek = Tasks.FistLetterUpper(date.ToString("dddd", lang));
-        //     if (semaine.Contains(nameOfWeek))
-        //     {
-        //         int col = semaine.IndexOf(nameOfWeek);
-        //
-        //         StackPanel stackPanel = new StackPanel()
-        //         {
-        //             Orientation = Orientation.Vertical,
-        //             HorizontalAlignment = HorizontalAlignment.Center
-        //         };
-        //
-        //         Brush foreground = dateNow == date ? Brushes.White : FindResource("RgbM2") as Brush;
-        //
-        //         // Label lbJourNom = new Label()
-        //         // {
-        //         //     Content = nameOfWeek,
-        //         //     Width = Constants.LabelWidthSize,
-        //         //     Height = Constants.LabelHeighSize,
-        //         //     FontSize = Constants.LabelFontSize,
-        //         //     Foreground = foreground,
-        //         //     HorizontalAlignment = HorizontalAlignment.Center,
-        //         // };
-        //         // Label lbNumJour = new Label()
-        //         // {
-        //         //     Content = $"{date.Day}",
-        //         //     Width = Constants.LabelWidthSize,
-        //         //     Height = Constants.LabelHeighSize,
-        //         //     FontSize = Constants.LabelFontSize,
-        //         //     Foreground = foreground,
-        //         //     HorizontalAlignment = HorizontalAlignment.Center,
-        //         // };
-        //
-        //         Label lbJourNom = makeLabel(content: nameOfWeek, foreground: foreground);
-        //         Label lbNumJour = makeLabel(content: $"{date.Day}", foreground: foreground);
-        //         
-        //         Grid gridDlg = new Grid();
-        //         int dlgRow = 0;
-        //
-        //         NpgsqlDataReader cdReader = Sql.GetDlgByDate(date: date.ToString("yyyy-MM-dd"));
-        //         while (cdReader.Read())
-        //         {
-        //             Button button = Widget.MakeBtnDlg(cdReader: cdReader, style: style);
-        //             Widget.SetElementGrid(button, row: dlgRow);
-        //
-        //             gridDlg.RowDefinitions.Add(new RowDefinition());
-        //             gridDlg.Children.Add(button);
-        //             dlgRow++;
-        //         }
-        //
-        //         cdReader.Close();
-        //
-        //         stackPanel.Children.Add(lbJourNom);
-        //         stackPanel.Children.Add(lbNumJour);
-        //         stackPanel.Children.Add(gridDlg);
-        //         
-        //         // Widget.SetElementGrid(lbJourNom, row:0);
-        //         // Widget.SetElementGrid(lbNumJour, row:1);
-        //         // Widget.SetElementGrid(gridDlg, row: 2);
-        //         
-        //         Widget.SetElementGrid(stackPanel, row:i, column:col);
-        //         GridMonth.Children.Add(stackPanel);
-        //     }
-        // }
     }
 
+    private StackPanel makePanel(DateTime date)
+    {
+        var style = FindResource("ButtonDLGTemp") as Style;
+        string nameOfWeek = Tasks.FistLetterUpper(date.ToString("dddd", lang));
+
+        StackPanel stackPanel = new StackPanel()
+        {
+            Orientation = Orientation.Vertical,
+            HorizontalAlignment = HorizontalAlignment.Center
+        };
+                
+        Brush foreground = dateNow == date ? Brushes.White : FindResource("RgbM2") as Brush;
+                
+        Label lbJourNom = makeLabel(content: nameOfWeek, foreground: foreground);
+        Label lbNumJour = makeLabel(content: $"{date.Day}", foreground: foreground);
+        
+        Grid gridDlg = new Grid();
+        int dlgRow = 0;
+        
+        NpgsqlDataReader cdReader = Sql.GetDlgByDate(date: date.ToString("yyyy-MM-dd"));
+        while (cdReader.Read())
+        {
+            Button button = Widget.MakeBtnDlg(cdReader: cdReader, style: style);
+            Widget.SetElementGrid(button, row: dlgRow);
+                
+            gridDlg.RowDefinitions.Add(new RowDefinition());
+            gridDlg.Children.Add(button);
+            dlgRow++;
+        }
+
+        ScrollViewer scrollViewer = new ScrollViewer()
+        {
+            Content = gridDlg,
+            MaxHeight = 200,
+            VerticalScrollBarVisibility = ScrollBarVisibility.Auto,
+            HorizontalScrollBarVisibility = ScrollBarVisibility.Disabled
+        };
+
+        // Border border = new Border()
+        // {
+        //     CornerRadius = new CornerRadius(5),
+        //     Background = Brushes.White,
+        //     Height = Constants.LabelHeighSize,
+        //     Width = Constants.LabelWidthSize,
+        //     Margin = new Thickness(5),
+        // };
+        // border.Child = border;
+                
+        cdReader.Close();
+                
+        stackPanel.Children.Add(lbJourNom);
+        stackPanel.Children.Add(lbNumJour);
+        stackPanel.Children.Add(scrollViewer);
+
+        return stackPanel;
+    }
     private Label makeLabel(string content, Brush foreground)
     {
         return new Label()
@@ -160,6 +116,7 @@ public partial class DlgViewMonth : UserControl
             FontSize = Constants.LabelFontSize,
             Foreground = foreground,
             HorizontalAlignment = HorizontalAlignment.Center,
+            VerticalAlignment = VerticalAlignment.Center,
         };
     }
 }
