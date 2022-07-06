673dfa15d5efc1408094d6c5a8b143e091587355
LaM0uette <lam0uette.dev@gmail.com>
2022-07-06 10:44:42 +0200

    menage dlgview autosize

--- Views/DlgView.xaml.cs
+++ Views/DlgView.xaml.cs
@@ -9,11 +9,10 @@ namespace GeoTools.Views;
 
 public partial class DlgView
 {
-    
-    private static TabItem _vTabItemDlgAll = new ();
-    private static TabItem _vTabItemDlgMonth = new ();
+    private static TabItem _vTabItemDlgAll = new();
+    private static TabItem _vTabItemDlgMonth = new();
     private static List<ToggleButton> _toggleButtons = new();
-    
+
     public DlgView()
     {
         InitializeComponent();
@@ -21,13 +20,13 @@ public partial class DlgView
         SetTabItems();
 
         Tasks.SetSelectedTabItem(_vTabItemDlgAll);
-        
+
         AddToggleButtonsInList();
-        
-        //ChangeWidth();
-        if (Application.Current.MainWindow != null) Application.Current.MainWindow.SizeChanged += OnSizeChanged;
+
+        // Detecte le changement de taille de la mainWindow
+        if (Application.Current.MainWindow != null) Application.Current.MainWindow.SizeChanged += OnWindowSizeChanged;
     }
-    
+
     //
     // Functions
     private void AddComboBoxData()
@@ -39,14 +38,14 @@ public partial class DlgView
         ComboBoxTypeView.Items.Insert(4, "MOIS");
         ComboBoxTypeView.SelectedItem = ComboBoxTypeView.Items.IndexOf("TOUT");
     }
-    
+
     private void AddToggleButtonsInList()
     {
         _toggleButtons.Add(BtnDlgAll);
         _toggleButtons.Add(BtnDlgATraiter);
         _toggleButtons.Add(BtnDlgFait);
     }
-    
+
     private void SetTabItems()
     {
         _vTabItemDlgAll = TabItemDlgAll;
@@ -55,64 +54,36 @@ public partial class DlgView
 
     //
     // Actions
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+    //
+    // Event
+    private void OnWindowSizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        //var maxHeigh = Tasks.GetWindowHeight() - (ActualHeight != double.NaN ? ActualHeight : 0);
+        var maxWith = Tasks.GetWindowWidth() - DlgLegend.ActualWidth - (
+            Tasks.GetWindowState() == WindowState.Maximized
+            ? Constants.MaximazeScrollBarWith
+            : Constants.NormalScrollBarWith
+            );
+
+        //DlgViewMonth.GridMonth.MaxHeight = maxHeigh;
+        DlgViewAll.Width = maxWith;
+        DlgViewMonth.Width = maxWith;
+    }
+
 
     private void BtnDlgBackHome_OnClick(object sender, RoutedEventArgs e)
     {
         Tasks.SetSelectedTabItem(MainView.VTabItemMenu);
     }
-    
-    private void OnSizeChanged(object sender, SizeChangedEventArgs e)
-    {
-        ChangeWidth();
-        ChangeHeight();
-    }
 
-    private void ChangeHeight()
-    {
-        double maxHeigh = GetMaxHeight();
-        
-        //DlgViewMonth.GridMonth.MaxHeight = maxHeigh;
-    }
-    private void ChangeWidth()
-    {
-        double maxWith = GetMaxWidth();
-        
-        DlgViewAll.Width = maxWith;
-        DlgViewMonth.Width = maxWith;
-    }
-
-    private double GetMaxHeight()
-    {
-        return Tasks.GetWindowHeight() - (ActualHeight != double.NaN ? ActualHeight : 0);
-    }
-    private double GetMaxWidth()
-    {
-        return Tasks.GetWindowWidth() - DlgLegend.ActualWidth - 
-               (Tasks.GetWindowState() == WindowState.Maximized?Constants.MaximazeScrollBarWith:Constants.NormalScrollBarWith);
-    }
-    
-    
-    
     void Toggle_OnClick(object sender, RoutedEventArgs e)
     {
         string btnName = ((ToggleButton) sender).Name;
         string mode = $"{((ToggleButton) sender).Content}";
 
-        DlgViewAll.InstanceDlgViewAll.CreateBtnDlgAll(mode:mode);
+        DlgViewAll.InstanceDlgViewAll.CreateBtnDlgAll(mode: mode);
 
         foreach (ToggleButton btn in _toggleButtons)
         {
