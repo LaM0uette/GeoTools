3dd679120c1838a26420f7b021f65969723fb697
LaM0uette <lam0uette.dev@gmail.com>
2022-08-01 14:11:03 +0200

    add day name and number

--- Views/Dlg/MonthDlgView.xaml.cs
+++ Views/Dlg/MonthDlgView.xaml.cs
@@ -3,6 +3,7 @@ using System.Windows;
 using System.Windows.Controls;
 using GeoTools.Utils;
 using Npgsql;
+using Parser;
 
 namespace GeoTools.Views.Dlg;
 
@@ -72,14 +73,37 @@ public partial class MonthDlgView
         // TODO: A MODIFIER !
         const int month = 6;
         const int year = 2022;
+        
         var lastWeek = 0;
-        var weekInc = -1;
+        var weekInc = 0;
+        
+        var lbLun = Widgets.NewDlgInfoTextBlock("Lundi", 18);
+        var lbMar = Widgets.NewDlgInfoTextBlock("Mardi", 18);
+        var lbMer = Widgets.NewDlgInfoTextBlock("Mercredi", 18);
+        var lbJeu = Widgets.NewDlgInfoTextBlock("Jeudi", 18);
+        var lbVen = Widgets.NewDlgInfoTextBlock("Vencdredi", 18);
+        SetGridItemsV(lbLun, 0, 0);
+        SetGridItemsV(lbMar, 1, 0);
+        SetGridItemsV(lbMer, 2, 0);
+        SetGridItemsV(lbJeu, 3, 0);
+        SetGridItemsV(lbVen, 4, 0);
+        
+        MonthGrid.RowDefinitions.Add(new RowDefinition());
+        MonthGrid.Children.Add(lbLun);
+        MonthGrid.Children.Add(lbMar);
+        MonthGrid.Children.Add(lbMer);
+        MonthGrid.Children.Add(lbJeu);
+        MonthGrid.Children.Add(lbVen);
+        
         
         for (var i = 1; i <= DateTime.DaysInMonth(year, month); i++)  // Boucle sur tous les jours du mois
         {
             var day = new DateTime(year, month, i);
             var weekOfTheDay = Tasks.GetWeekNumber(day);
             var sp = Widgets.NewMonthDlgStackPanel($"{i:D2}{month:D2}{year}");
+            var dayName = Widgets.NewDlgInfoTextBlock(i.ParseToString(), 20);
+            sp.MinWidth = Constants.Dlg.MonthWidth;
+            dayName.HorizontalAlignment = HorizontalAlignment.Left;
 
             // Check if is new week
             if (weekOfTheDay > lastWeek)
@@ -117,10 +141,17 @@ public partial class MonthDlgView
             }
 
             MonthGrid.Children.Add(sp);
+            sp.Children.Add(dayName);
         }
     }
     
     private static void SetGridItems(StackPanel sp, int col, int row)
+    {
+        Grid.SetColumn(sp, col);
+        Grid.SetRow(sp, row);
+    }
+    
+    private static void SetGridItemsV(TextBlock sp, int col, int row)
     {
         Grid.SetColumn(sp, col);
         Grid.SetRow(sp, row);
